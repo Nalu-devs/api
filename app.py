6d08c94c5c44796c4a6ca76147401a43be5ebb41
alunoEtec <aula@etec.com>
2026-04-02 14:08:08 -0300

    teste

--- app.py
+++ app.py
@@ -2,27 +2,38 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-alunos = [
+usuarios = [
     {"id": 1, "nome": "Ana"},
-    {"id": 2, "nome": "Carlos"},
-    {"id": 3, "nome": "Carol"}
+    {"id": 2, "nome": "João"}
 ]
 
-@app.route("/")
-def home():
-    return "API Flask rodando!"
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify(usuarios)
 
-@app.route("/alunos", methods=["GET"])
-def listar_alunos():
-    return jsonify(alunos)
-
-@app.route("/alunos", methods=["POST"])
-def criar_alunos():
+@app.route('/usuarios', methods=['POST'])
+def criar_usuario():
     novo = request.json
-    novo['id'] = len(alunos) + 1
-    alunos.append(novo)
+    novo['id'] = len(usuarios) + 1
+    usuarios.append(novo)
     return jsonify(novo), 201
 
+@app.route('/usuarios/<int:id>', methods=['PUT'])
+def atualizar_usuario(id):
+    for usuario in usuarios:
+        if usuario['id'] == id:
+            dados = request.json
+            usuario['nome'] = dados.get('nome', usuario['nome'])
+            return jsonify(usuario)
+    return {"erro": "Usuário não encontrado"}, 404
 
-if __name__ == "__main__":
+@app.route('/usuarios/<int:id>', methods=['DELETE'])
+def deletar_usuario(id):
+    for usuario in usuarios:
+        if usuario['id'] == id:
+            usuarios.remove(usuario)
+            return {"mensagem": "Usuário removido"}
+    return {"erro": "Usuário não encontrado"}, 404
+
+if __name__ == '__main__':
     app.run(debug=True)

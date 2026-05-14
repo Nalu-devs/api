0fb234640eacc532afd563b9c180fb6e1f3f30c5
Aluno.Etec <aula@etec.com>
2026-05-13 22:07:47 -0300

    fix: serve index.html na raiz e arquivos estaticos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ CORS(app)
 
 @app.route("/")
 def inicio():
-    return "API FUNFO"
+    return send_from_directory(app.root_path, "index.html")
 
 usuarios = [
    
@@ -76,6 +76,10 @@ usuarios = [
 @app.route('/usuarios', methods=['GET'])
 def listar_usuarios():
     return jsonify(usuarios)
+
+@app.route("/<path:filename>")
+def static_files(filename):
+    return send_from_directory(app.root_path, filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
